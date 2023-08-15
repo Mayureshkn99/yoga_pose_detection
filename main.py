7d91eccf14c3159ac78656f42acdfa23ef405d5c
Mayuresh <mayureshkn99@gmail.com>
2023-08-15 19:39:12 -0400

    docs: updated requirements

--- main.py
+++
@@ -1,9 +0,0 @@
-import utils
-import joblib
-
-if __name__ == "__main__":
-    model = joblib.load('model.joblib')
-    file_path = 'uploads/image.png'  # include filepath from Flask uploaded image
-    keypoints = utils.extract_keypoints(file_path)
-    result = model.predict([keypoints])[0]  # display this on the results page
-    print(result)
