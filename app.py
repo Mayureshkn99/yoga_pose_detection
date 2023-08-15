c1e3b1064fbf6541f1d16d6ea3666272efc881a9
Mayuresh <mayureshkn99@gmail.com>
2023-08-15 14:35:29 -0400

    feat: added driver code

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
+import utils
+import matplotlib.pyplot as plt
+import numpy as np
+import joblib
+import random
+
 if __name__ == "__main__":
-    pass
+    model = joblib.load('model.joblib')
+    file_path = 'uploads/image.png'  # include filepath from Flask uploaded image
+    keypoints = utils.extract_keypoints(file_path)
+    result = model.predict([keypoints])[0]  # display this on the results page
+    print(result)
