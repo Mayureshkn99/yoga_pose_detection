39294b3946253c9db23c51008452a6841f409b71
Mayuresh <mayureshkn99@gmail.com>
2023-08-15 17:21:00 -0400

    feat: Flask integration

--- app.py
+++ app.py
@@ -1,12 +1,59 @@
+import os
 import utils
-import matplotlib.pyplot as plt
-import numpy as np
 import joblib
-import random
+from flask import Flask, render_template, request
+
+app = Flask(__name__, static_folder="images")
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Define the allowed file extensions for uploaded images
+ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png'}
+
+TARGET = os.path.join(APP_ROOT, 'images/')
+
+if not os.path.isdir(TARGET):
+    os.mkdir(TARGET)
+
+MODEL = joblib.load('model.joblib')
+
+# Function to check if a filename has an allowed extension
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('upload.html')
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Check if the post request has the file part
+        if 'file' not in request.files:
+            return "No file part"
+
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an empty file without a filename
+        if file.filename == '':
+            return "No selected file"
+        
+        # Check if the file has an allowed extension
+        if file and allowed_file(file.filename):
+            dest = '/'.join([TARGET, file.filename])
+            file.save(dest)
+
+        keypoints = utils.extract_keypoints(dest)
+        if len(keypoints) != 33*4:
+            detection = False
+            result = None
+        else:
+            result = MODEL.predict([keypoints])[0]
+            detection = True
+
+        return render_template('complete.html', image_name=file.filename, detection=detection, result=result)
+    return render_template('upload.html')
 
 if __name__ == "__main__":
-    model = joblib.load('model.joblib')
-    file_path = 'uploads/image.png'  # include filepath from Flask uploaded image
-    keypoints = utils.extract_keypoints(file_path)
-    result = model.predict([keypoints])[0]  # display this on the results page
-    print(result)
+    app.run(port=5000, debug=True)
--- main.py
+++ main.py
@@ -1,36 +1,9 @@
-import os
-from flask import Flask, render_template, request
-from predictor import check
+import utils
+import joblib
 
-app = Flask(__name__, static_folder="images")
-
-APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('upload.html')
-
-
-@app.route('/upload', methods=['GET', 'POST'])
-def upload():
-    target = os.path.join(APP_ROOT, 'images/')
-    print(target)
-
-    if not os.path.isdir(target):
-        os.mkdir(target)
-
-    for file in request.files.getlist('file'):
-        print(file)
-        filename = file.filename
-        print(filename)
-        dest = '/'.join([target, filename])
-        print(dest)
-        file.save(dest)
-
-    status = check(filename)
-
-    return render_template('complete.html', image_name=filename, predvalue=status)
-
-if __name__ == "main":
-    app.run(port=5000, debug=True)
+if __name__ == "__main__":
+    model = joblib.load('model.joblib')
+    file_path = 'uploads/image.png'  # include filepath from Flask uploaded image
+    keypoints = utils.extract_keypoints(file_path)
+    result = model.predict([keypoints])[0]  # display this on the results page
+    print(result)
