6a12e8a999356d9a0be6702a0f1b401e9cc2a2a7
taspips <tasneempipalyawala@gmail.com>
2023-08-15 16:17:08 -0400

    Flask helper code updated
    Flask helper code updated

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+from flask import Flask, render_template, request
+from predictor import check
+
+app = Flask(__name__, static_folder="images")
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('upload.html')
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    target = os.path.join(APP_ROOT, 'images/')
+    print(target)
+
+    if not os.path.isdir(target):
+        os.mkdir(target)
+
+    for file in request.files.getlist('file'):
+        print(file)
+        filename = file.filename
+        print(filename)
+        dest = '/'.join([target, filename])
+        print(dest)
+        file.save(dest)
+
+    status = check(filename)
+
+    return render_template('complete.html', image_name=filename, predvalue=status)
+
+if __name__ == "main":
+    app.run(port=5000, debug=True)
